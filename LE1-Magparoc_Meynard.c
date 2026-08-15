98d72fc0f363428d698fa2a5f065faaccfdbda48
magparocmeynard-IT1 <magparoc.meynard08@gmail.com>
2026-08-15 20:58:18 +0800

    Add files via upload

---
+++ LE1-Magparoc_Meynard.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main() {
+    printf("========================================\n");
+    printf("            STUDENT PROFILE             \n");
+    printf("========================================\n\n");
+
+    printf("Name              : Meynard Magparoc\n");
+    printf("Course & Section  : BSIT-1R14\n");
+    printf("Year Level        : 1st Year\n\n");
+
+    printf("========================================\n");
+    printf("            FUTURE PROGRAMMER            \n");
+    printf("========================================\n");
+
+    return 0;
+}
---
+++ Magparoc_Activity2_WELCOME SCREEN.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+    printf("==========================================\n");
+    printf("||                                      ||\n");
+    printf("||        WELCOME TO C PROGRAMMING      ||\n");
+    printf("||                                      ||\n");
+    printf("||         LEARN - CODE - CREATE        ||\n");
+    printf("||                                      ||\n");
+    printf("||              BSIT-1R14               ||\n");
+    printf("||              1ST YEAR                ||\n");
+    printf("||                                      ||\n");
+    printf("==========================================\n");
+
+    return 0;
+}
